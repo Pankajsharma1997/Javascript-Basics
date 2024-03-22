6b6ca82969241ca2f20a4654d20b0207b91db554
Pankaj sharam <Pankajsharma1997>
2024-03-22 17:32:07 +0530

    objects in JS

---
+++ object/index.js
@@ -0,0 +1,16 @@
+// JavaScript Object
+
+const user = {
+    name: 'Pankaj Sharma',
+    age : 25,
+    hobby: 'Playing Chess',
+}
+console.log(user);
+
+// Access a Perticuler item from the object via two types 1 is dot notation , break notation 
+
+// Access the name from the user object using dot notation 
+console.log(user.name);
+
+// Access the age from the user object using the break notation 
+console.log(user['age']);
