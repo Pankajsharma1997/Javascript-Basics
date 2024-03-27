9fb6e60dcd750b85775e2405bb14babe0b3e2d01
Pankaj sharam <Pankajsharma1997>
2024-03-27 18:46:37 +0530

    object adding , deleting, updating properties

--- object/index.js
+++ object/index.js
@@ -1,4 +1,4 @@
-// JavaScript Object
+// JavaScript Object & javaScript Manipulation 
 
 const user = {
     name: 'Pankaj Sharma',
@@ -7,10 +7,25 @@ const user = {
 }
 console.log(user);
 
-// Access a Perticuler item from the object via two types 1 is dot notation , break notation 
+// Access a Perticuler item from the object via two types first  is dot notation , break notation 
 
 // Access the name from the user object using dot notation 
 console.log(user.name);
 
 // Access the age from the user object using the break notation 
 console.log(user['age']);
+
+
+// Add 
+user.email = 'pankajsharmabca16@gmail.com'  // Add the email in the user object using the dot notation 
+user['phone'] = 8894597426 
+
+// Update  update the phone number
+user.phone = 8988235880 
+
+
+// Delete 
+delete user.age;
+
+
+console.log(user);
